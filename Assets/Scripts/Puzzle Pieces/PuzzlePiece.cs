9b9e23ffe8ffca35353d43e4a833df60229150bc
Covina <jabs303@gmail.com>
2017-10-08 00:21:24 -0700

    Section 5, Lecture 45:  Additional setup for game controllers and managers.

---
+++ Assets/Scripts/Puzzle Pieces/PuzzlePiece.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzlePiece {
+
+    // Current location of puzzle piece
+    public int CurrentRow { get; set; }
+    public int CurrentColumn { get; set; }
+
+
+    // Original locations of the puzzle piece
+    // this is before it gets split up and shuffled
+    public static int OriginalRow { get; set; }
+    public static int OriginalColumn { get; set; }
+
+    // This individual puzzle piece
+    public static GameObject GameObject { get; set; }
+
+
+}
---
+++ Assets/Scripts/Variables/GameVariables.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameVariables {
+
+
+    // Number of puzzle rows
+    public static int MaxRows = 4;
+
+    // Number of puzzle columns
+    public static int MaxColumns = 4;
+
+    // Total number of individual puzzle pieces
+    public static int MaxSize = MaxRows * MaxColumns;
+
+
+
+}
+
+/// <summary>
+/// Possible game states
+/// </summary>
+public enum GameState
+{
+    Playing,
+    Animating,
+    End
+}
